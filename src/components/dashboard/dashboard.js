215bd0cd6ece8bd5da1dc2e37c01807e3ccd2181
Chris.Chen <Chris.Chen@splashtop.com>
2024-06-27 19:03:03 +0800

    begin 主頁按鈕router

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -8,6 +8,7 @@ export default function board() {
     return (
       <div>
         <h1>My App</h1>
+        <button type="button" class="btn btn-success" >work out ! </button>
       </div>
     );
   }
