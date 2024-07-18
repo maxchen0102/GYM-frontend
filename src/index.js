5265b9229ad45cbc6821de9940d38dc1a5d2c05e
Chris.Chen <Chris.Chen@splashtop.com>
2024-07-18 16:30:38 +0800

    登出完成

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Layout from './layout';
 import './index.css';
+import axios from 'axios';
+
+// 设置 Axios 的全局配置
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
